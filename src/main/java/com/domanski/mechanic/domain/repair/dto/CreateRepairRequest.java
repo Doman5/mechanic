330c0297529611431e2ef12bd7769f1de374973b
Dominik <dono3108@gmail.com>
2023-04-12 14:03:02 +0200

    add validation to CreateRepairRequest and PartsAndWorkTimeRequest

--- src/main/java/com/domanski/mechanic/domain/repair/dto/CreateRepairRequest.java
+++ src/main/java/com/domanski/mechanic/domain/repair/dto/CreateRepairRequest.java
@@ -2,7 +2,14 @@ package com.domanski.mechanic.domain.repair.dto;
 
 import lombok.Builder;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Builder
-public record CreateRepairRequest(String description,
-                                  Long userId) {
+public record CreateRepairRequest(
+        @NotBlank
+        @NotNull
+        String description,
+        @NotNull
+        Long userId) {
 }
