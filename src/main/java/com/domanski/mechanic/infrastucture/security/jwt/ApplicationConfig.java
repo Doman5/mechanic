755abf50034900b286be021068fe64d9f04b0750
Dominik <dono3108@gmail.com>
2023-04-22 22:34:41 +0200

    create configuration classes

---
+++ src/main/java/com/domanski/mechanic/infrastucture/security/jwt/ApplicationConfig.java
@@ -0,0 +1,41 @@
+package com.domanski.mechanic.infrastucture.security.jwt;
+
+import com.domanski.mechanic.domain.loginandregister.LoginAndRegisterFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(LoginAndRegisterFacade loginAndRegisterFacade) {
+        return loginAndRegisterFacade::findByUsername;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(LoginAndRegisterFacade loginAndRegisterFacade) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService(loginAndRegisterFacade));
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
