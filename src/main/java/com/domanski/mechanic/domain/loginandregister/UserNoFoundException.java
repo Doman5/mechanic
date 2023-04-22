bd2892aed1da163025e7b6f05065e0dea606df3a
Dominik <dono3108@gmail.com>
2023-04-22 22:20:46 +0200

    delete redundant dto, mapper and exception

--- src/main/java/com/domanski/mechanic/domain/loginandregister/UserNoFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.domanski.mechanic.domain.loginandregister;
-
-class UserNoFoundException extends RuntimeException{
-    public UserNoFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/domanski/mechanic/domain/loginandregister/dto/RegisterUserResponse.java
+++
@@ -1,6 +0,0 @@
-package com.domanski.mechanic.domain.loginandregister.dto;
-
-public record RegisterUserResponse(Long id,
-                                   String username,
-                                   boolean isRegistered) {
-}
